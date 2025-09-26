71320e868a1c60be2b65edbe05f514626e81f89c
BitsHost <dadryan26@gmail.com>
2025-09-26 22:07:26 +0300

    Initial professional version of OC Filter extension for OpenCart.

---
+++ admin/controller/extension/module/oc_filter.php
@@ -0,0 +1,10 @@
+<?php
+// OC Filter extension for OpenCart
+// Initial professional version
+
+class ControllerExtensionModuleOcFilter extends Controller {
+    public function index() {
+        // Your code here
+    }
+}
+?>
